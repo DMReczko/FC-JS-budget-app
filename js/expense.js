4399bd5251e2cd30a3082403664d5b4489c12632
DMReczko <117109879+DMReczko@users.noreply.github.com>
2023-02-22 18:24:56 +0100

    sniff

--- js/expense.js
+++ js/expense.js
@@ -7,76 +7,79 @@ const expenseList = document.querySelector("#expense-list");
 const expenseValue = document.querySelector("#expense-value");
 
 const deleteExpense = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    expenseList.removeChild(event.target.parentNode);
+  expenseList.removeChild(event.target.parentNode);
 
-    sumupExpense();
-    budgetTally();
+  sumupExpense();
+  budgetTally();
 };
 
 const editElement = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const button = event.target;
-    const parentNode = button.parentNode;
-    const expenseName = parentNode.querySelector(".expense-name");
-    const expenseValue = parentNode.querySelector(".expense-amount");
+  const button = event.target;
+  const parentNode = button.parentNode;
+  const expenseName = parentNode.querySelector(".expense-name");
+  const expenseValue = parentNode.querySelector(".expense-amount");
 
-    if (isEditMode(expenseName, expenseValue)) {
-        if (isNaN(expenseValue.textContent)) {
-            return alert("Wprowadź poprawną kwotę");
-        } else {
-            expenseValue.textContent = parseFloat(
-                expenseValue.textContent
-            ).toFixed(2);
-            expenseName.contentEditable = "false";
-            expenseValue.contentEditable = "false";
-            button.innerHTML = "Edytuj";
-
-            expenseName.classList.remove("input-edit");
-            expenseValue.classList.remove("input-edit");
-        }
+  if (isEditMode(expenseName, expenseValue)) {
+    if (
+      isNaN(expenseValue.textContent) |
+      (expenseValue.textContent.trim() == "")
+    ) {
+      return alert("Wprowadź poprawną kwotę");
     } else {
-        expenseName.contentEditable = "true";
-        expenseValue.contentEditable = "true";
-        button.innerHTML = "Zapisz";
-    }
+      expenseValue.textContent = parseFloat(expenseValue.textContent).toFixed(
+        2
+      );
+      expenseName.contentEditable = "false";
+      expenseValue.contentEditable = "false";
+      button.innerHTML = "Edytuj";
 
-    sumupExpense();
-    budgetTally();
+      expenseName.classList.remove("input-edit");
+      expenseValue.classList.remove("input-edit");
+    }
+  } else {
+    expenseName.contentEditable = "true";
+    expenseValue.contentEditable = "true";
+    button.innerHTML = "Zapisz";
+  }
+
+  sumupExpense();
+  budgetTally();
 };
 
 const isEditMode = (expenseName, expenseValue) => {
-    expenseName.classList.add("input-edit");
-    expenseValue.classList.add("input-edit");
+  expenseName.classList.add("input-edit");
+  expenseValue.classList.add("input-edit");
 
-    return (
-        (expenseName.contentEditable == "true") |
-        (expenseValue.contentEditable == "true")
-    );
+  return (
+    (expenseName.contentEditable == "true") |
+    (expenseValue.contentEditable == "true")
+  );
 };
 
 export const addExpense = () => {
-    const expenseName = addExpenseText.value;
-    const expenseAmount = parseFloat(expenseValue.value).toFixed(2);
+  const expenseName = addExpenseText.value;
+  const expenseAmount = parseFloat(expenseValue.value).toFixed(2);
 
-    if (expenseAmount && expenseAmount > 0 && expenseName.trim()) {
-        const expenseListElem = document.createElement("li");
-        const deleteBtn = createBtn("Usuń", "delete-btn", deleteExpense);
-        const editBtn = createBtn("Edytuj", "edit-btn", editElement);
+  if (expenseAmount && expenseAmount > 0 && expenseName.trim()) {
+    const expenseListElem = document.createElement("li");
+    const deleteBtn = createBtn("Usuń", "delete-btn", deleteExpense);
+    const editBtn = createBtn("Edytuj", "edit-btn", editElement);
 
-        expenseListElem.id = Math.floor(Math.random() * 10000);
-        expenseListElem.innerHTML = `<div class="expense-name">${expenseName}</div><div class="expense-amount">${expenseAmount}</div>`;
-        expenseListElem.appendChild(editBtn);
-        expenseListElem.appendChild(deleteBtn);
-        expenseListElem.id = Math.floor(Math.random() * 10000);
+    expenseListElem.id = Math.floor(Math.random() * 10000);
+    expenseListElem.innerHTML = `<div class="expense-name">${expenseName}</div><div class="expense-amount">${expenseAmount}</div>`;
+    expenseListElem.appendChild(editBtn);
+    expenseListElem.appendChild(deleteBtn);
+    expenseListElem.id = Math.floor(Math.random() * 10000);
 
-        expenseList.appendChild(expenseListElem);
+    expenseList.appendChild(expenseListElem);
 
-        addExpenseText.value = "";
-        expenseValue.value = "";
-    } else {
-        alert("Wpisz poprawne wartości w oba pola");
-    }
+    addExpenseText.value = "";
+    expenseValue.value = "";
+  } else {
+    alert("Wpisz poprawne wartości w oba pola");
+  }
 };
--- js/income.js
+++ js/income.js
@@ -7,76 +7,77 @@ const incomeList = document.querySelector("#income-list");
 const incomeValue = document.querySelector("#income-value");
 
 const deleteIncome = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    incomeList.removeChild(event.target.parentNode);
+  incomeList.removeChild(event.target.parentNode);
 
-    sumupIncome();
-    budgetTally();
+  sumupIncome();
+  budgetTally();
 };
 
 const editElement = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const button = event.target;
-    const parentNode = button.parentNode;
-    const incomeName = parentNode.querySelector(".income-name");
-    const incomeValue = parentNode.querySelector(".income-amount");
+  const button = event.target;
+  const parentNode = button.parentNode;
+  const incomeName = parentNode.querySelector(".income-name");
+  const incomeValue = parentNode.querySelector(".income-amount");
 
-    if (isEditMode(incomeName, incomeValue)) {
-        if (isNaN(incomeValue.textContent)) {
-            return alert("Wprowadź poprawną kwotę");
-        } else {
-            incomeValue.textContent = parseFloat(
-                incomeValue.textContent
-            ).toFixed(2);
-            incomeName.contentEditable = "false";
-            incomeValue.contentEditable = "false";
-            button.innerHTML = "Edytuj";
-
-            incomeName.classList.remove("input-edit");
-            incomeValue.classList.remove("input-edit");
-        }
+  if (isEditMode(incomeName, incomeValue)) {
+    if (
+      isNaN(incomeValue.textContent) |
+      (incomeValue.textContent.trim() == "")
+    ) {
+      return alert("Wprowadź poprawną kwotę");
     } else {
-        incomeName.contentEditable = "true";
-        incomeValue.contentEditable = "true";
-        button.innerHTML = "Zapisz";
-    }
+      incomeValue.textContent = parseFloat(incomeValue.textContent).toFixed(2);
+      incomeName.contentEditable = "false";
+      incomeValue.contentEditable = "false";
+      button.innerHTML = "Edytuj";
 
-    sumupIncome();
-    budgetTally();
+      incomeName.classList.remove("input-edit");
+      incomeValue.classList.remove("input-edit");
+    }
+  } else {
+    incomeName.contentEditable = "true";
+    incomeValue.contentEditable = "true";
+    button.innerHTML = "Zapisz";
+  }
+
+  sumupIncome();
+  budgetTally();
 };
 
 const isEditMode = (incomeName, incomeValue) => {
-    incomeName.classList.add("input-edit");
-    incomeValue.classList.add("input-edit");
+  incomeName.classList.add("input-edit");
+  incomeValue.classList.add("input-edit");
 
-    return (
-        (incomeName.contentEditable == "true") |
-        (incomeValue.contentEditable == "true")
-    );
+  return (
+    (incomeName.contentEditable == "true") |
+    (incomeValue.contentEditable == "true")
+  );
 };
 
 export const addIncome = () => {
-    const incomeName = addIncomeText.value;
-    const incomeAmount = parseFloat(incomeValue.value).toFixed(2);
+  const incomeName = addIncomeText.value;
+  const incomeAmount = parseFloat(incomeValue.value).toFixed(2);
 
-    if (incomeAmount && incomeAmount > 0 && incomeName.trim()) {
-        const incomeListElem = document.createElement("li");
-        const deleteBtn = createBtn("Usuń", "delete-btn", deleteIncome);
-        const editBtn = createBtn("Edytuj", "edit-btn", editElement);
+  if (incomeAmount && incomeAmount > 0 && incomeName.trim()) {
+    const incomeListElem = document.createElement("li");
+    const deleteBtn = createBtn("Usuń", "delete-btn", deleteIncome);
+    const editBtn = createBtn("Edytuj", "edit-btn", editElement);
 
-        incomeListElem.id = Math.floor(Math.random() * 10000);
-        incomeListElem.innerHTML = `<div class="income-name">${incomeName}</div><div class="income-amount">${incomeAmount}</div>`;
-        incomeListElem.appendChild(editBtn);
-        incomeListElem.appendChild(deleteBtn);
-        incomeListElem.id = Math.floor(Math.random() * 10000);
+    incomeListElem.id = Math.floor(Math.random() * 10000);
+    incomeListElem.innerHTML = `<div class="income-name">${incomeName}</div><div class="income-amount">${incomeAmount}</div>`;
+    incomeListElem.appendChild(editBtn);
+    incomeListElem.appendChild(deleteBtn);
+    incomeListElem.id = Math.floor(Math.random() * 10000);
 
-        incomeList.appendChild(incomeListElem);
+    incomeList.appendChild(incomeListElem);
 
-        addIncomeText.value = "";
-        incomeValue.value = "";
-    } else {
-        alert("Wpisz poprawne wartości w oba pola");
-    }
+    addIncomeText.value = "";
+    incomeValue.value = "";
+  } else {
+    alert("Wpisz poprawne wartości w oba pola");
+  }
 };
